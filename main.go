9a20f181cf3faf8ab3debf9814b65cf5415d5fcb
Ilya <pila200207@gmail.com>
2021-12-18 10:36:24 +0300

    message

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	for {
+		var s string
+		fmt.Print("enter your question")
+		fmt.Scanf("%s", &s)
+
+		switch s {
+		case "name":
+			fmt.Println("Max")
+		case "age":
+			fmt.Printf("I am 24")
+		default:
+			fmt.Println("What are u talking about?")
+
+		}
+
+		fmt.Scanln()
+
+	}
+}

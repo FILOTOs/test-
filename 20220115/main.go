ac5886c78e13f3ac3da7b7bbb9dcfa5c00d4edb2
Ilya <pila200207@gmail.com>
2022-01-23 18:27:59 +0300

    phoneLookup

---
+++ 20220115/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type PhoneLookup struct {
+	Numbers map[string]Person
+}
+
+func NewPhoneLookup() PhoneLookup {
+	return PhoneLookup{
+		Numbers: map[string]Person{},
+	}
+}
+
+func (p *PhoneLookup) AddNumber(phone, name string, age int) {
+	p.Numbers[phone] = Person{
+		Name: name,
+		Age:  age,
+	}
+}
+
+func (p *PhoneLookup) GetNumber(phone string) Person {
+	if person, ok := p.Numbers[phone]; ok {
+		return person
+	} else {
+		fmt.Println("not found")
+		return Person{}
+	}
+}
+
+func add() error {
+	// input phone number
+	var phone string
+	fmt.Print("input number: ")
+	fmt.Scanf("%s\n", &phone)
+	// check phone is correct
+	if matched, _ := regexp.MatchString("([0-9]|\\+)+", phone); matched {
+		return fmt.Errorf("wrong phone")
+	}
+
+	fmt.Print("input name: ")
+	name, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	name = strings.ToLower(strings.Replace(name, "\n", "", -1))
+	// check phone is correct
+	if matched, _ := regexp.MatchString("([A-Za-z])+", name); matched {
+		return fmt.Errorf("wrong name")
+	}
+
+	var age int
+	fmt.Print("input age: ")
+	fmt.Scanf("%d\n", &age)
+	if age < 10 {
+		return fmt.Errorf("wrong age")
+	}
+
+	phoneLookup.AddNumber(phone, name, age)
+
+	return nil
+}
+
+func get() {
+	var phone string
+	fmt.Print("input number: ")
+	fmt.Scanf("%s\n", &phone)
+
+	if person := phoneLookup.GetNumber(phone); person.Name != "" {
+		fmt.Printf("name: %s, age: %d\n", person.Name, person.Age)
+	}
+}
+
+var phoneLookup = NewPhoneLookup()
+
+func main() {
+
+	for {
+		var cmd string
+		fmt.Println("\nInput command: ")
+		fmt.Scanf("%s", &cmd)
+
+		switch cmd {
+		case "add":
+			if err := add(); err != nil {
+				fmt.Println("error:", err.Error())
+			}
+		case "get":
+			get()
+		case "exit":
+			fmt.Println("bye")
+			return
+		default:
+			fmt.Println("unknown command")
+		}
+	}
+
+}

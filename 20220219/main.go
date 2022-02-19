cbd3246484c4df8a5459c6072bbe314c1a4f95c4
Ilya <pila200207@gmail.com>
2022-02-19 11:45:05 +0300

    tree

---
+++ 20220219/main.go
@@ -0,0 +1,78 @@
+package main
+
+import "fmt"
+
+type node struct {
+	nodeId       int
+	parentNodeId *int
+	val          string
+}
+
+type tree struct {
+	nodes []*node
+}
+
+func (t *tree) addNode(n *node) error {
+
+	if n.parentNodeId != nil {
+		found := false
+		for _, node := range t.nodes {
+			if node.nodeId == *n.parentNodeId {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return fmt.Errorf("parentNode not found")
+		}
+	}
+
+	t.nodes = append(t.nodes, n)
+
+	return nil
+}
+
+func (t *tree) print() {
+	for _, n := range t.nodes {
+		if n.parentNodeId == nil {
+			fmt.Printf("nodeId: %d, parentNodeId: %v, val: %s\n", n.nodeId, nil, n.val)
+		} else {
+			fmt.Printf("nodeId: %d, parentNodeId: %v, val: %s\n", n.nodeId, *n.parentNodeId, n.val)
+		}
+	}
+}
+
+func (t *tree) printPretty(level int, parentNodeId *int) {
+	for _, n := range t.nodes {
+		if (n.parentNodeId == nil && parentNodeId == nil) || (n.parentNodeId != nil && parentNodeId != nil && *n.parentNodeId == *parentNodeId) {
+			fmt.Printf("%"+fmt.Sprintf("%ds", 10)+"\n", n.val)
+			t.printPretty(level+1, &n.nodeId)
+		}
+	}
+}
+
+func newTree() *tree {
+	return &tree{}
+}
+
+func main() {
+	tree := newTree()
+	rootNodeId := 1
+	tree.addNode(&node{
+		nodeId: rootNodeId,
+		val:    "root",
+	})
+
+	tree.addNode(&node{
+		nodeId:       2,
+		parentNodeId: &rootNodeId,
+		val:          "node2",
+	})
+	tree.addNode(&node{
+		nodeId:       3,
+		parentNodeId: &rootNodeId,
+		val:          "node3",
+	})
+
+	tree.printPretty(0, nil)
+}
